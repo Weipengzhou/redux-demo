a09da3c827ad62c5cc9c9666a09b12ce3b210ff2
weipengzhou <492020314@qq.com>
2018-07-30 00:01:10 +0800

    del some  things with redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,8 @@
 import { applyMiddleware, createStore} from 'redux';
-import createSagaMiddleware from 'redux-saga';
 import reducers from './reducer';
 
-
-const sagaMiddleware = createSagaMiddleware();
 export default function configureStore() {
-    const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+    const store = createStore(reducers, applyMiddleware());
      
     return store;
 }   
